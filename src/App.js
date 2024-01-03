954ffee362c1ba3b312cae1f5613071e0e6913b9
nilaxsuvisinth <nilaxsuvisinth27@gmail.com>
2024-01-03 11:59:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { useEffect, useState } from "react";
+import TodoItem from "./TodoItem";
+
+const API_BASE = 'http://localhost:4001/todo';
 
 function App() {
+  
+  const [items, setItems] = useState([]);
+
+  // Add input state, we will store the user's input in this state
+  const [input, setInput] = useState("");
+
+  useEffect(() => {
+    GetTodos();
+  }, []);
+
+// Store the target's value into the input state 
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  }
+
+  const GetTodos = () => {
+    fetch(API_BASE)
+      .then(res => res.json())
+      .then(data => setItems(data))
+      .catch(err => console.log(err))
+  }
+  const addItem = async() => {
+    const data = await fetch(API_BASE + "/new", {
+     method: "POST",
+     headers: {
+       "content-type" : "application/json"
+     },
+     body: JSON.stringify({
+       name: input,
+         })
+    }).then(res => res.json()) 
+    await GetTodos()
+    setInput('')
+   }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ // The input field's value is now taken from the input state. 
+ //It will update dynamically as the user types.
+ // Add the onChange method, so that handleChange function will be executed
+ // every time something has been changed in the input field.
+
+    <div className="container">
+      <div className="heading">
+        <h1>TO-DO-APPLICATION</h1>
+      </div>
+
+      <div className="form">
+        <input type='text' value={input} onChange={handleChange}></input>
+        <button onClick={()=>addItem()}>
+ <span>ADD</span>
+ </button>
+      </div>
+ 
+      <div className="todolist">  
+      {items.map((item)=> {
+        const {_id, name} = item
+        return  <TodoItem name={name} id={_id} setItems={setItems}/>   
+      })
+    }
+      </div>
     </div>
+ 
   );
 }
 
